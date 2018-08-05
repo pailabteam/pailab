8fcd5a696e9a0ffbbfdcdda5d4ae00359fbc6d99
Daniel Oeltz <d.oeltz@rivacon.com>
2018-08-05 06:48:17 +0200

    add MLObjectTypes

--- repo/repo.py
+++ repo/repo.py
@@ -6,6 +6,14 @@ import logging
 LOGGER = logging.getLogger('repo')
 
 
+class MLObjectType(Enum):
+    """Enums describing all ml object types.
+    """
+    TRAINING_DATA = 'training_data'
+    TEST_DATA = 'test_data'
+    MODEL_PARAMETER = 'model_parameter'
+    TRAINING_PARAMETER = 'training_parameter'
+
 
 
 
