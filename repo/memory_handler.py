f21cb14956f1e755dd2877c7f2772ab3c132b412
Daniel Oeltz <d.oeltz@rivacon.com>
2018-08-06 16:51:25 +0200

    add memory handler for numpy objects

--- repo/memory_handler.py
+++ repo/memory_handler.py
@@ -12,7 +12,6 @@ class RepoObjectMemoryStorage:
         :param category: A string describing the category.
         :param name: Name (as string) of object
         :param obj: The object added.
-
         """
         if not category in self._store.keys():
             self._store[category] = {}
@@ -33,3 +32,29 @@ class RepoObjectMemoryStorage:
         if version >= len(tmp):
             raise Exception('No valid versionnumber (' + str(version) + ') for object ' + name + ' in category ' + category)
         return self._store[category][name][version]
+
+class NumpyMemoryStorage:
+    def __init__(self):
+        self._store = {}
+
+    def add(self, name, version, numpy_dict):
+        """ Add numpy data from an object to the storage.
+
+        :param name: Name (as string) of object
+        :param version: object version
+        :param numpy_dict: numpy dictionary
+
+        """
+        if not name in self._store.keys():
+            self._store[name] = {version : numpy_dict}
+        else:
+            self._store[name][version] = numpy_dict
+
+    def get(self, name, version):
+        """
+        """
+        if not name in self._store.keys():
+            raise Exception('No numpy data for object ' + name + ' with version ' + str(version))
+        if not version in self._store[name].keys():
+            raise Exception('No numpy data for object ' + name + ' with version ' + str(version))
+        return self._store[name][version]
