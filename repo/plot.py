8fff5c1c3f9eb89d27a8099d9dbec221b7438f0d
Daniel Oeltz <d.oeltz@rivacon.com>
2018-09-29 07:03:31 +0200

    add more convenience to measure vs param plotting

--- repo/plot.py
+++ repo/plot.py
@@ -1,6 +1,7 @@
 import logging
 import pandas as pd
 import repo.plot_helper as plot_helper
+from repo.repo import NamingConventions
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import plotly.graph_objs as go
 logger = logging.getLogger('repo.plot')
@@ -8,26 +9,38 @@ logger = logging.getLogger('repo.plot')
 init_notebook_mode(connected=True)
 
 
-def measure_by_model_parameter(ml_repo, measure_name, param_name):
+def measure_by_model_parameter(ml_repo, measure_name, param_name, data_versions=None):
     measures = plot_helper.get_measure_by_model_parameter(
-        ml_repo, measure_name, param_name)
-    hover_text = ["model version: " +
-                  str(x['model_version']) for x in measures]
+        ml_repo, measure_name, param_name, data_versions)
+    data_name = str(NamingConventions.Data(
+        NamingConventions.EvalData(NamingConventions.Measure(measure_name))))
+    data_versions = set()
+    for measure in measures:
+        data_versions.add(measure['data_version'])
     measures = pd.DataFrame(measures)
+    data = []
 
-    data = [
-        go.Scatter(
-            x=[i for i in range(len(measures.index))],
-            y=measures['value'],
-            text=hover_text,
-            name=measure_name
+    for d_version in data_versions:
+        df = measures.loc[measures['data_version'] == d_version]
+        data.append(
+            go.Scatter(
+                x=df[param_name],  # [i for i in range(len(measures.index))],
+                y=df['value'],
+                text=["model version: " +
+                      str(x['model_version']) +
+                      '<br>data_version: ' + str(x['data_version'])
+                      for index, x in df.iterrows()],
+                name=data_name + ': ' + str(d_version),
+                mode='markers'
+            )
+
         )
-    ]
 
     layout = go.Layout(
         title='measure by parameter',
         xaxis=dict(title=param_name),
-        yaxis=dict(title=measure_name)
+        yaxis=dict(title=NamingConventions.Measure(
+            measure_name).values['measure_type'])
     )
     # IPython notebook
     # py.iplot(data, filename='pandas/basic-line-plot')
