b798331c14aa7c29b92e3c76ba371b3ce087a6a2
Daniel Oeltz <d.oeltz@rivacon.com>
2018-11-02 15:36:27 +0100

    coverage for pailab only

--- run_coverage.bat
+++ run_coverage.bat
@@ -1,5 +1,5 @@
 REM del coverage.xml
-coverage run ./tests/repo_test.py
-coverage run -a ./tests/repo_disk_storage_test.py
-coverage run -a ./tests/SQLiteJobRunner_test.py
+coverage run --source=pailab ./tests/repo_test.py
+coverage run --source=pailab -a ./tests/repo_disk_storage_test.py
+coverage run --source=pailab -a ./tests/SQLiteJobRunner_test.py
 coverage html
