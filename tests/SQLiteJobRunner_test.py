a058463e5dce5ab05a49aad891fbe2142c587a67
Daniel Oeltz <d.oeltz@rivacon.com>
2018-11-02 07:19:27 +0100

    new unittests for SQLite job_runner

--- tests/SQLiteJobRunner_test.py
+++ tests/SQLiteJobRunner_test.py
@@ -27,6 +27,29 @@ class TestClass:
         return self.a + self._b
 
 
+def eval_func_test(model, data):
+    '''Dummy model eval function for testing
+
+        Function retrns independent of data and model a simple numpy array with zeros
+    Arguments:
+        model {} -- dummy model, not used
+        data {} -- dummy data, not used
+    '''
+    return np.zeros([10, 1])
+
+
+def train_func_test(model_param, training_param, data):
+    '''Dummy model training function for testing
+
+        Function retrns independent of data and model a simple numpy array with zeros
+    Arguments:
+        model_param {} -- dummy model parameter
+        training_param {} -- dummy training parameter, not used
+        data {} -- dummy trainig data, not used
+    '''
+    return TestClass(2, 3, repo_info={})  # pylint: disable=E1123
+
+
 class SQLiteJobRunner_Test(unittest.TestCase):
 
     def _setup_measure_config(self):
@@ -60,7 +83,8 @@ class SQLiteJobRunner_Test(unittest.TestCase):
 
         self.repository = repo.MLRepo(
             'doeltz', self.handler, numpy_handler, self.handler, None)
-        self.job_runner = SQLiteJobRunner('tmp/job_runner.py', self.repository)
+        self.job_runner = SQLiteJobRunner(
+            'tmp/job_runner.sqlite', self.repository)
         self.repository._job_runner = self.job_runner
         # Setup dummy RawData
         raw_data = RawData(np.zeros([10, 1]), ['x0'], np.zeros(
@@ -81,9 +105,10 @@ class SQLiteJobRunner_Test(unittest.TestCase):
                               repo_info={repo_objects.RepoInfoKey.NAME.value: 'test_data_2',  repo_objects.RepoInfoKey.CATEGORY: repo.MLObjectType.TEST_DATA})
         self.repository.add([training_data, test_data_1, test_data_2])
 
-        self.repository.add_eval_function('tests.repo_test', 'eval_func_test')
+        self.repository.add_eval_function(
+            'tests.SQLiteJobRunner_test', 'eval_func_test')
         self.repository.add_training_function(
-            'tests.repo_test', 'train_func_test')
+            'tests.SQLiteJobRunner_test', 'train_func_test')
         self.repository.add(TestClass(1, 2, repo_info={repo_objects.RepoInfoKey.NAME.value: 'training_param',  # pylint: disable=E1123
                                                        repo_objects.RepoInfoKey.CATEGORY: repo.MLObjectType.TRAINING_PARAM}))
         # setup dummy model definition
@@ -103,16 +128,37 @@ class SQLiteJobRunner_Test(unittest.TestCase):
             pass
 
     def test_job_runner(self):
-        job = self.repository.run_training()
+        def count_waiting_pred(jobs):
+            n_waiting_pred = 0
+            for job in jobs:
+                job_info = self.job_runner.get_info(job[0], job[1])
+                if job_info['job_state'] == JobState.WAITING_PRED.value:
+                    n_waiting_pred += 1
+            return n_waiting_pred
+
+        job = self.repository.run_training(run_descendants=True)
         tmp = self.job_runner.get_info(job[0], job[1])
         # check if traning job is in waiting state
         self.assertEqual(tmp['job_state'], JobState.WAITING.value)
-        self.job_runner.run(1)
-        tmp = self.job_runner.get_info(job[0], job[1])
-        self.assertEqual(tmp['job_state'],
-                         JobState.SUCCESSFULLY_FINISHED.value)
+        waiting_jobs = self.job_runner.get_waiting_jobs()
+        n_waiting_jobs = len(waiting_jobs)
+        # count the number of jobs waiting for preprocessing
+        n_waiting_pred = count_waiting_pred(waiting_jobs)
+        self.assertEqual(n_waiting_jobs, 10)
+        for i in range(n_waiting_jobs):
+            self.job_runner.run(1)
+            if i == 0:
+                tmp = self.job_runner.get_info(job[0], job[1])
+                self.assertEqual(tmp['job_state'],
+                                 JobState.SUCCESSFULLY_FINISHED.value)
+            waiting_jobs = self.job_runner.get_waiting_jobs()
+            n_waiting_pred = count_waiting_pred(waiting_jobs)
+            self.assertEqual(n_waiting_jobs-1,
+                             len(waiting_jobs))
+            self.assertGreater(n_waiting_jobs, n_waiting_pred)
+            n_waiting_jobs = len(waiting_jobs)
 
-        self.assertEqual(0, 0)
+        # now count th jobs waiting for predecessors
 
 
 if __name__ == '__main__':
