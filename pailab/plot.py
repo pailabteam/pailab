83851152d9beae53d2078e31a42772beb30fec16
Daniel Oeltz <d.oeltz@rivacon.com>
2018-11-13 15:18:26 +0100

    plot more then one feature

--- pailab/plot.py
+++ pailab/plot.py
@@ -11,33 +11,39 @@ init_notebook_mode(connected=True)
 
 
 def measure_by_model_parameter(ml_repo, measure_name, param_name, data_versions=None):
-    measures = plot_helper.get_measure_by_model_parameter(
+    x = plot_helper.get_measure_by_model_parameter(
         ml_repo, measure_name, param_name, data_versions)
-    data_name = str(NamingConventions.Data(
-        NamingConventions.EvalData(NamingConventions.Measure(measure_name))))
-    data_versions = set()
-    for measure in measures:
-        data_versions.add(measure['data_version'])
-    measures = pd.DataFrame(measures)
-    data = []
+    for k, measures in x.items():
+        data_name = str(NamingConventions.Data(
+            NamingConventions.EvalData(NamingConventions.Measure(measure_name))))
+        data_versions = set()
+        for measure in measures:
+            data_versions.add(measure['data_version'])
+        measures = pd.DataFrame(measures)
+        data = []
 
-    for d_version in data_versions:
-        # if True:
-        df = measures.loc[measures['data_version'] == d_version]
-        text = ["model version: " + str(x['model_version']) + '<br>' +
-                data_name + ': ' + str(x['data_version']) + '<br>'
-                + 'train_data: ' + str(x['train_data_version'])
-                for index, x in df.iterrows()]
-        data.append(
-            go.Scatter(
-                x=df[param_name],
-                y=df['value'],
-                text=text,
-                name=data_name + ': ' + str(d_version),
-                mode='markers'
+        for d_version in data_versions:
+            # if True:
+            df = measures.loc[measures['data_version'] == d_version]
+            text = ["model version: " + str(x['model_version']) + '<br>' +
+                    data_name + ': ' + str(x['data_version']) + '<br>'
+                    + 'train_data: ' + str(x['train_data_version'])
+                    for index, x in df.iterrows()]
+
+            if len(x) > 1:
+                plot_name = k + ': ' + str(d_version)
+            else:
+                plot_name = data_name + ': ' + str(d_version)
+            data.append(
+                go.Scatter(
+                    x=df[param_name],
+                    y=df['value'],
+                    text=text,
+                    name=plot_name,
+                    mode='markers'
+                )
+
             )
-
-        )
 
     layout = go.Layout(
         title='measure by parameter',
--- pailab/plot_helper.py
+++ pailab/plot_helper.py
@@ -5,33 +5,57 @@ from pailab.repo_store import RepoStore  # pylint: disable=E0401
 logger = logging.getLogger('repo.plot')
 
 
-def get_measure_by_model_parameter(ml_repo, measure_name, param_name, data_versions=None):
+def get_measure_by_model_parameter(ml_repo, measure_names, param_name, data_versions=None):
+    """Returns for a (list of) measure(s) the measures and corresponding param values for a certain parameter 
+
+    Args:
+        ml_repo (MLRepo): the ml repo
+        measure_names (str, list(str)): string or list of strings of measure names  
+        param_name (str): name of parameter
+        data_versions (version number, optional): Defaults to None. If not None, only values on measures on dta with this version number are used
+
+    Raises:
+        NotImplementedError: raises exception if data_versiosn is not None
+
+    Returns:
+        [dict]: dictionary of measure name to list of dictionaries containing the result, i.e. 
+        model_version: version of model parameter
+        param_version: version of the parameter of this data point
+        param_name: the parameter value
+        data_version: version of the underlying data
+        train_data_version: version number of trainin data used to calibrate the model leading to this measure
+        measure_version: version of measure
+        value: measure value
+    """
+
+    if isinstance(measure_names, str):
+        measure_names = [measure_names]
     if data_versions is not None:
         raise NotImplementedError()
-    measures = ml_repo.get(measure_name, version=(
-        RepoStore.FIRST_VERSION, RepoStore.LAST_VERSION))
-    # else:
-    #    measures = ml_repo.get(measure_name, version=(
-    #        RepoStore.FIRST_VERSION, RepoStore.LAST_VERSION), )
-    model_name = NamingConventions.CalibratedModel(
-        NamingConventions.Measure(measure_name)
-    )
-    data = str(NamingConventions.Data(NamingConventions.EvalData(
-        NamingConventions.Measure(measure_name))))
-    model_param_name = str(NamingConventions.ModelParam(model_name))
-    train_data = ml_repo.get_names(MLObjectType.TRAINING_DATA)[0]
-    model_name = str(model_name)
-    # eval_name
+    result_all = {}
+    for measure_name in measure_names:
+        measures = ml_repo.get(measure_name, version=(
+            RepoStore.FIRST_VERSION, RepoStore.LAST_VERSION))
+        model_name = NamingConventions.CalibratedModel(
+            NamingConventions.Measure(measure_name)
+        )
+        data = str(NamingConventions.Data(NamingConventions.EvalData(
+            NamingConventions.Measure(measure_name))))
+        model_param_name = str(NamingConventions.ModelParam(model_name))
+        train_data = ml_repo.get_names(MLObjectType.TRAINING_DATA)[0]
+        model_name = str(model_name)
+        # eval_name
 
-    result = []
-    for x in measures:
-        p = ml_repo.get(
-            model_param_name, version=x.repo_info[RepoInfoKey.MODIFICATION_INFO][model_param_name])
-        param_value = p.get_params()[param_name]
-        info = {'model_version': x.repo_info[RepoInfoKey.MODIFICATION_INFO][model_name],
-                param_name: param_value, 'param_version': p.repo_info[RepoInfoKey.VERSION],
-                'data_version': x.repo_info[RepoInfoKey.MODIFICATION_INFO][data],
-                'train_data_version': x.repo_info[RepoInfoKey.MODIFICATION_INFO][train_data],
-                'measure_version': x.repo_info[RepoInfoKey.VERSION], 'value': x.value}
-        result.append(info)
-    return result
+        result = []
+        for x in measures:
+            p = ml_repo.get(
+                model_param_name, version=x.repo_info[RepoInfoKey.MODIFICATION_INFO][model_param_name])
+            param_value = p.get_params()[param_name]
+            info = {'model_version': x.repo_info[RepoInfoKey.MODIFICATION_INFO][model_name],
+                    param_name: param_value, 'param_version': p.repo_info[RepoInfoKey.VERSION],
+                    'data_version': x.repo_info[RepoInfoKey.MODIFICATION_INFO][data],
+                    'train_data_version': x.repo_info[RepoInfoKey.MODIFICATION_INFO][train_data],
+                    'measure_version': x.repo_info[RepoInfoKey.VERSION], 'value': x.value}
+            result.append(info)
+        result_all[measure_name] = result
+    return result_all
