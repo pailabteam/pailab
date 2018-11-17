18f98d3a6b15a1446921ccba1e9a179e0272f0bf
Daniel Oeltz <d.oeltz@rivacon.com>
2018-11-17 07:26:11 +0100

    add path_to_names

--- pailab/tools.py
+++ pailab/tools.py
@@ -5,6 +5,72 @@ from pailab.repo_store import RepoStore  # pylint: disable=E0401
 logger = logging.getLogger(__name__)
 
 
+class _ObjNames:
+    def __init__(self, names=None, sep='/'):
+        if isinstance(names, list):
+            for x in names:
+                self.__add(x)
+        else:
+            if isinstance(names, dict):
+                for k, v in names.items():
+                    self.__add(v, k.split(sep))
+
+    def __add(self, name, name_separated=None, sep='/'):
+        if name_separated is None:
+            name_separated = name.split(sep)
+        if len(name_separated) == 1:
+            leaf = _ObjNames()
+            setattr(leaf, '__name', name)
+            setattr(self, name_separated[0], leaf)
+        else:
+            if hasattr(self, name_separated[0]):
+                d = getattr(self, name_separated[0])
+                d.__add(name, name_separated[1:])
+            else:
+                sub_obj = _ObjNames()
+                sub_obj.__add(name, name_separated[1:])
+                setattr(self, name_separated[0], sub_obj)
+
+    def __call__(self, containing_str=None):
+        if len(self.__dict__) == 1:
+            for k, v in self.__dict__.items():
+                if isinstance(v, str):
+                    return v
+                else:
+                    return v()
+        else:
+            result = []
+            for k, v in self.__dict__.items():
+                if isinstance(v, str):
+                    result.append(v)
+                else:
+                    d = v()
+                    if isinstance(d, str):
+                        result.append(d)
+                    else:
+                        result.extend(d)
+            if containing_str is not None:
+                return [x for x in result if containing_str in x]
+            return result
+
+
+def path_to_names(repo, include_object_types=[MLObjectType.MEASURE, MLObjectType.CALIBRATED_MODEL,
+                                              MLObjectType.EVAL_DATA, MLObjectType.JOB, MLObjectType.LABEL,
+                                              MLObjectType.MODEL_PARAM, MLObjectType.TEST_DATA, MLObjectType.TRAINING_DATA,
+                                              MLObjectType.TRAINING_PARAM]):
+    """Returns an object which allows easy access to the names of all repo objects
+
+    Args:
+        repo ([type]): [description]
+    """
+    obj_names = []
+    for k in include_object_types:
+        names = repo.get_names(k.value)
+        obj_names.extend(names)
+    repo_obj = _ObjNames(obj_names)
+    return repo_obj
+
+
 def check_model(repo, model_name, correct=False, model_version=RepoStore.LAST_VERSION):
     """Check if the model is calibrated and evaluated on the latest versions
 
