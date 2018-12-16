3469046ea89226b939b19544270d192e6acdc01a
Daniel Oeltz <d.oeltz@rivacon.com>
2018-12-16 19:43:21 +0100

    add RepoStoreFactory

---
+++ pailab/repo_store_factory.py
@@ -0,0 +1,17 @@
+
+class RepoStoreFactory:
+
+    @staticmethod
+    def get_repo_stores():
+        return ['disk_handler', 'git_handler']
+
+    @staticmethod
+    def get(repo_store_type, **kwargs):
+        if repo_store_type == 'disk_handler':
+            from pailab.disk_handler import RepoObjectDiskStorage
+            return RepoObjectDiskStorage(**kwargs)
+        elif repo_store_type == 'git_handler':
+            from pailab.git_handler import RepoObjectGitStorage
+            return RepoObjectGitStorage(**kwargs)
+        raise Exception('Cannot create RepoStore: Unknown repo type ' + repo_store_type +
+                        '. Use only types from the list returned by RepoStoreFactory.get_repo_stores().')
