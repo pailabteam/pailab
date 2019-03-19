3bf45e62c57bf138d817935f4950f2d5ea61e2ae
Daniel Oeltz <d.oeltz@rivacon.com>
2019-03-19 08:34:31 +0100

    fix issues with pull for git_handler

--- pailab/ml_repo/git_handler.py
+++ pailab/ml_repo/git_handler.py
@@ -13,8 +13,14 @@ logger = logging.getLogger(__name__)
 
 
 class RepoObjectGitStorage(RepoObjectDiskStorage):
+    """Object storage with git support.
+
+    This storge stores all objects on disk  in a local git storage. It provides functionality to push and pull from 
+    another git repo. Note that it handles all files in the same way as RepoObjectDiskStorage does (using methods from this storage).
+    """
+
     @staticmethod
-    def is_git_repo(path):
+    def _is_git_repo(path):
         try:
             _ = Repo(path).git_dir
             return True
@@ -30,18 +36,16 @@ class RepoObjectGitStorage(RepoObjectDiskStorage):
             load_function (function, optional): Defaults to pickle_load. Function used to load the objects from disk.
         """
         super(RepoObjectGitStorage, self).__init__(**kwargs)
-        if RepoObjectGitStorage.is_git_repo(self._main_dir):
-            self._git_repo = Repo(self._main_dir)
-        else:
-            self._git_repo = Repo.init(self._main_dir)
-        if not os.path.isfile(self._main_dir+'/.gitignore'):
-            with open(self._main_dir+'/.gitignore', 'w') as f:
-                f.write('.version.sqlite')
-
+        # initialize git repo if it does not exist
+        if not RepoObjectGitStorage._is_git_repo(self._main_dir):
+            _ = Repo.init(self._main_dir)
+        
     def _add(self, obj):
         super(RepoObjectGitStorage, self)._add(obj)
-        self.commit(obj['repo_info']
-                    [repo_objects.RepoInfoKey.COMMIT_MESSAGE.value])
+        message = 'no message'
+        if obj['repo_info']['commit_message'] is not None:
+            message = obj['repo_info']['commit_message']
+        self.commit(message)
 
     def _delete(self, name, version):
         super(RepoObjectGitStorage, self)._delete(name, version)
@@ -62,15 +66,14 @@ class RepoObjectGitStorage(RepoObjectDiskStorage):
         if len(check) > 0:
             raise Exception(
                 "Integrity check fails, cannot commit: " + str(check))
-        try:
-            self._git_repo.git.add('-A')
-            self._git_repo.git.commit('-m', message)
-        except:
-            pass
+        git_repo = Repo(self._main_dir)
+        git_repo.git.add('-A')
+        git_repo.git.commit('-m', message)
 
     def push(self, remote_name='origin'):
+        _git_repo = Repo(self._main_dir)
         remote = None
-        for r in self._git_repo.remotes:
+        for r in _git_repo.remotes:
             if r.name == remote_name:
                 remote = r
                 break
@@ -83,16 +86,18 @@ class RepoObjectGitStorage(RepoObjectDiskStorage):
         c.execute('ATTACH DATABASE "' + sqlite_db_2 + '" AS db_2')
         statement = 'INSERT OR IGNORE INTO versions(name, version, file, uuid_time) SELECT name, version, file, uuid_time FROM db_2.versions;'
         c.execute(statement)
-        statement = 'INSERT OR IGNORE INTO mapping(name, vcategory) SELECT name, category FROM db_2.mapping;'
+        statement = 'INSERT OR IGNORE INTO mapping(name, category) SELECT name, category FROM db_2.mapping;'
         c.execute(statement)
         statement = 'INSERT OR IGNORE INTO modification_info(name, version, modifier, modifier_version) SELECT name, version, modifier, modifier_version FROM db_2.modification_info;'
         c.execute(statement)
+        self._conn.commit()
         c.execute("DETACH DATABASE 'db_2';")
 
     def pull(self, remote_name='origin'):
-        self._conn.close()
+        # self._conn.close()
         remote = None
-        for r in self._git_repo.remotes:
+        _git_repo = Repo(self._main_dir)
+        for r in _git_repo.remotes:
             if r.name == remote_name:
                 remote = r
                 break
@@ -102,9 +107,9 @@ class RepoObjectGitStorage(RepoObjectDiskStorage):
         os.rename(self._sqlite_db_name(), self._sqlite_db_name() + '_old')
         try:
             remote.pull()
-        except e:
+        except:
             os.rename(self._sqlite_db_name() + '_old', self._sqlite_db_name())
             raise Exception('An error occured during pull: ' + (str(e)))
-        self._merge_from_db(self._sqlite_db_name(),
-                            self._sqlite_db_name() + '_old')
+        self._conn = sqlite3.connect(self._sqlite_db_name())
+        self._merge_from_db(self._sqlite_db_name() + '_old')
         os.remove(self._sqlite_db_name() + '_old')
